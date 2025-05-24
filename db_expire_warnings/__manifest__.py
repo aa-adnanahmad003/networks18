f7fa3b8308eebec9b657f69c01baec847dbf689e
aa-adnanahmad003 <89076891+aa-adnanahmad003@users.noreply.github.com>
2025-05-25 02:44:28 +0500

    Database Expiry

---
+++ db_expire_warnings/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "DB Expire Warnings",
+    "description": "With the help of this module, all of DB warnings will be hidden. This will works for Enterprise "
+                   "Editions",
+    "summary": "With the help of this module, all of DB warnings will be hidden. This will works for Enterprise "
+               "Editions",
+    "author": "Farooq Butt | 03015902110",
+    "website_url": "https://itsoftcreations.com/",
+    'version': '18.0.2.0.1',
+    "maintainer": "Farooq Butt",
+    "license": "LGPL-3",
+    "sequence": 100,
+    "assets": {
+        "web.assets_backend": [
+            "db_expire_warnings/static/src/scss/db_expire_warnings.scss"
+        ]
+    },
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+}
